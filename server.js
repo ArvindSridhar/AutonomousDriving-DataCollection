13ef7dbf5702e480f24da0b9b506f39dd6d2ce89
Arvind Sridhar <arvindsridhar@berkeley.edu>
2018-02-24 12:04:30 -0800

    MongoDB functionality added

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ var path = require('path');
 var app = express();
 var fs = require('fs')
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = 'mongodb://arvindsridhar:addeepl3arning92@ds225028.mlab.com:25028/autonomousdriving';
+const dbName = 'autonomousdriving';
+
 app.set('port', 3000);
 app.use(express.static(path.join(__dirname, '/frontend')));
 
 app.get('/send', function(req, res) {
 	var data = req.param("name");
+    var video = req.param("url");
     data = JSON.parse(data);
-    console.log(data[0]);
+    console.log(video);
+    
+    MongoClient.connect(url, function(err, client) {
+        assert.equals(err, null);
+        console.log("Connected successfully to server");
+        var myobj = { name: video, clickLogs: data };
+        const db = client.db(dbName);
+        db.collection("timestamps").insertOne(myobj, function(err, res) {
+            assert.equals(err, null);
+            console.log("1 document inserted");
+        });
+        db.close();
+    });
+    
 	res.send(req.body);
 });
 
--- server.py
+++
@@ -1,7 +0,0 @@
-from flask import *
-app = Flask(__name__)
- 
-@app.route("/")
-def index():
-    #url_for('static', filename='app.css')
-    return render_template('index.html')
