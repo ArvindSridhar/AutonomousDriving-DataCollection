09ad060c1eaed60c9d63f6c682316e1cbbdd5403
Arvind Sridhar <arvindsridhar@berkeley.edu>
2018-03-03 00:47:48 -0800

    Better UI

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-var fs = require('fs')
+var fs = require('fs');
+//var id;
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
@@ -15,21 +16,23 @@ app.get('/send', function(req, res) {
 	var data = req.param("name");
     var video = req.param("url");
     data = JSON.parse(data);
-    console.log(video);
+    //console.log(video);
     
     MongoClient.connect(url, function(err, client) {
-        assert.equals(err, null);
-        console.log("Connected successfully to server");
+        //assert.equals(err, null);
+        //console.log("Connected successfully to server");
         var myobj = { name: video, clickLogs: data };
         const db = client.db(dbName);
-        db.collection("timestamps").insertOne(myobj, function(err, res) {
-            assert.equals(err, null);
-            console.log("1 document inserted");
+        db.collection("timestamps").insertOne(myobj, function(err, res2) {
+            //assert.equals(err, null);
+            //console.log(res.insertedId);
+            id = res2.insertedId;
+            console.log(id)
+            res.send(id.toString());
         });
-        db.close();
+        //db.close();
     });
-    
-	res.send(req.body);
+    //console.log(id)
 });
 
 var server = app.listen(app.get('port'), function() {
